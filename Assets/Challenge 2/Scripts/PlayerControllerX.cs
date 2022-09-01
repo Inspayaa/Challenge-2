9d44fff34afd3ae50bd799c25882bc5a7741a5a9
Inspayaa <tobechukwujoel@gmail.com>
2022-09-01 12:43:07 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float timeDelay = 2.0f;
+    bool doneWaiting = false;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog, prevent spam, delay by 2 seconds
+        if (timeDelay > 0)
+        {
+            timeDelay -= Time.deltaTime;
+        }
+        
+        else
+        {
+            doneWaiting = true;
+            if (Input.GetKeyDown(KeyCode.Space) && doneWaiting)
+            {
+                getDogPrefab();
+               timeDelay = 2;
+            }
+        }
+        
+        
+        void getDogPrefab()
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+
+    }
+
+
+}
